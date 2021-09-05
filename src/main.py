0b1a747ef4f239046522edb361986ba0c853f2e5
gutessitore <gugatessi@gmail.com>
2021-09-05 20:04:33 -0300

    Remove headless option and add olx data

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from selenium.webdriver.chrome.options import Options
 from src.scrapper.trovit import get_trovit_data
+from src.scrapper.olx import get_olx_data
 from utils.utils import save_raw_data
 
-address = "avenida santo amaro"
+address = "rua monte alegre"
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 
 trovit_data = get_trovit_data(address, chrome_options)
+olx_data = get_olx_data(address, chrome_options)
 
 save_raw_data(trovit_data, "trovit-santo_amaro.json")
+save_raw_data(olx_data, "olx-santo_amaro.json")
