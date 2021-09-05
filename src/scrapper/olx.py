b88ecb4166a2a475d445bfd74562fd4cccb325ee
gutessitore <gugatessi@gmail.com>
2021-09-05 20:03:35 -0300

    Build get_olx_data

--- src/scrapper/olx.py
+++ src/scrapper/olx.py
@@ -1,35 +1,90 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from urllib.parse import quote
 from src.utils.utils import *
+from tqdm import tqdm
+import selenium
+import warnings
+import time
 
+MAX_DELAY = 10
 
 def announcement_parser(text: str) -> dict:
     bedroom_text_pattern = "(\d)(\sou mais)? quartos?"
     area_text_pattern = "(\d+)m²"
     parking_slot = "(\d+)(\sou mais)? vagas?"
+    price_text_pattern = "R\$\s?([\d{1,3}\.?]+)"
 
     real_state_dict = {
         "quartos": get_regex_group_from_pattern(text, bedroom_text_pattern),
         "área": get_regex_group_from_pattern(text, area_text_pattern),
         "vagas": get_regex_group_from_pattern(text, parking_slot),
-        "texto": text
+        "preço": get_regex_group_from_pattern(text, price_text_pattern)
     }
 
     return real_state_dict
 
 
-address = "rua monte alegre, SP"
-address_query = quote(address)
+def build_url_with_address(address: str, rent_only=True) -> str:
+    url_safe_address = quote(address)
+    base_url = "https://www.olx.com.br/imoveis"
 
-url_with_query = f"https://www.olx.com.br/imoveis?q={address_query}"
+    if rent_only:
+        base_url += "/aluguel"
 
-driver = init_driver()
-driver.get(url_with_query)
-
-announcement_info_class = "sc-1j5op1p-0"
-announcement_elements = driver.find_elements_by_class_name(announcement_info_class)
-parsed_announcement_text = [announcement_parser(an.text) for an in announcement_elements]
-
-print(json.dumps(parsed_announcement_text, indent=4))
+    return base_url + "q=" + url_safe_address
 
 
-driver.quit()
+def get_announcement_elements(driver: Chrome) -> list:
+    announcement_card_class = "sc-1fcmfeb-2"
+
+    element_present = EC.presence_of_element_located((By.CLASS_NAME, announcement_card_class))
+    WebDriverWait(driver, MAX_DELAY).until(element_present)
+
+    return driver.find_elements_by_class_name(announcement_card_class)
+
+
+def collect_element_data_by_class_name(element, class_name):
+    try:
+        return element.find_element_by_class_name(class_name).text
+    except selenium.common.exceptions.NoSuchElementException:
+        return "0"
+
+
+def get_announcement_data(elements: list) -> list:
+    announcement_info_class = "sc-1j5op1p-0"
+    announcement_address_class = "sc-7l84qu-1"
+    announcement_price_class = "aoie8y-0"
+
+    announcements_data = list()
+    for element in tqdm(elements):
+        announcement_info = collect_element_data_by_class_name(element, announcement_info_class)
+        announcement_address = collect_element_data_by_class_name(element, announcement_address_class)
+        announcement_price = collect_element_data_by_class_name(element, announcement_price_class)
+
+        announcement_data = announcement_parser(announcement_info + " " + announcement_price)
+        announcement_data["endereço"] = announcement_address
+        announcement_data["texto"] = element.text
+
+        announcements_data.append(announcement_data)
+
+    return announcements_data
+
+
+def get_olx_data(address: str, driver_options: Options = None) -> str:
+
+    chrome = init_driver(driver_options)
+    url_with_query = build_url_with_address(address)
+    chrome.get(url_with_query)
+    try:
+        real_state_elements = get_announcement_elements(chrome)
+        real_state_parsed_data = get_announcement_data(real_state_elements)
+    except Exception as e:
+        warnings.warn(e)
+        real_state_parsed_data = None
+
+    finally:
+        chrome.quit()
+
+    return real_state_parsed_data
