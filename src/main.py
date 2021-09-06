269b0c54a32714aefe2befcde4f0f7fae1c143ca
gutessitore <gugatessi@gmail.com>
2021-09-06 09:47:49 -0300

    Refactor main scrapper pipeline

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.chrome.options import Options
+from src.scrapper.zapimoveis import get_zapimoveis_data
 from src.scrapper.trovit import get_trovit_data
 from src.scrapper.olx import get_olx_data
 from utils.utils import save_raw_data
@@ -6,10 +7,28 @@ from utils.utils import save_raw_data
 address = "rua monte alegre"
 
 chrome_options = Options()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 
-trovit_data = get_trovit_data(address, chrome_options)
-olx_data = get_olx_data(address, chrome_options)
+scrappers = {
+    "olx": {
+        "function": get_olx_data,
+    },
+    "trovit": {
+        "function": get_trovit_data
+    },
+    "zapimoveis": {
+        "function": get_zapimoveis_data
+    }
+}
 
-save_raw_data(trovit_data, "trovit-santo_amaro.json")
-save_raw_data(olx_data, "olx-santo_amaro.json")
+for scrapper in scrappers.keys():
+    print("_"*30)
+    print(f"collecting data from {scrapper}")
+
+    scrapper_function = scrappers[scrapper]["function"]
+    scrapper_data = scrapper_function(address, chrome_options)
+
+    filename = f"{scrapper}-{address}.json"
+    print(f"saving data to {filename}")
+
+    save_raw_data(scrapper_data, filename)
