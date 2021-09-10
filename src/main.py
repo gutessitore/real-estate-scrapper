3c58d1aa7e3279f64454b6309ef389222b5a4f34
gutessitore <gugatessi@gmail.com>
2021-09-10 11:44:16 -0300

    Add execution time log

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ from database.firebase_manager import get_firebase_data
 from database.firebase_manager import upload_json
 from utils.utils import get_repo_absolute_path
 from collector import scrape_sites
+import time
 import os
 
-address = "rua monte alegre"
+address = "Rua Monte Alegre, Perdizes, SP"
 repo_path = get_repo_absolute_path()
 api_key = os.path.join(repo_path, "src", "database", "real-estate-scrapper-firebase.json")
 database_url = "https://real-estate-scrapper-2ac0c-default-rtdb.firebaseio.com/"
@@ -12,15 +13,15 @@ database_url = "https://real-estate-scrapper-2ac0c-default-rtdb.firebaseio.com/"
 
 firebase_data = get_firebase_data(api_key, database_url, address)
 
+start_time = time.time()
 if firebase_data is None:
-    print("data not found on database")
     scrape_sites(address)
-
     json_path = os.path.join(repo_path, "data", "processed", "data.json")
-    print("uploading to database")
     upload_json(api_key, database_url, json_path, address)
-    print("uploaded to database")
+
 else:
     print(firebase_data)
 
+total_time = time.time() - start_time
+print(f"Execution time: {total_time:.3f} secs")
 
