dfd5f9ec95b67bbf334640e0b0adb94dcb36e563
gutessitore <gugatessi@gmail.com>
2021-09-10 11:44:42 -0300

    Feat site scrapper is now async

--- src/collector.py
+++ src/collector.py
@@ -4,10 +4,11 @@ from src.scrapper.vivareal import get_vivareal_data
 from src.scrapper.trovit import get_trovit_data
 from src.scrapper.olx import get_olx_data
 from utils.utils import save_raw_data
+import concurrent.futures
 import pandas as pd
 
 
-def scrape_sites(address: str):
+def scrape_sites(address: str, save_local_csv=False, save_local_json=False):
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
@@ -15,33 +16,54 @@ def scrape_sites(address: str):
     scrappers = {
         "olx": {
             "function": get_olx_data,
+            "address": address,
+            "option": chrome_options
         },
         "trovit": {
-            "function": get_trovit_data
+            "function": get_trovit_data,
+            "address": address,
+            "option": chrome_options
         },
         "zapimoveis": {
-            "function": get_zapimoveis_data
+            "function": get_zapimoveis_data,
+            "address": address,
+            "option": chrome_options
         },
         "vivareal": {
-            "function": get_vivareal_data
+            "function": get_vivareal_data,
+            "address": address,
+            "option": chrome_options
         }
     }
 
-    data = list()
-    for scrapper in scrappers.keys():
-        print("_"*30)
-        print(f"collecting data from {scrapper}")
+    number_of_elements = len(scrappers.keys())
+    with concurrent.futures.ThreadPoolExecutor(max_workers=number_of_elements) as executor:
+        data = list(
+            executor.map(
+                collect_data_from_site,
+                scrappers.items()
+            )
+        )
 
-        scrapper_function = scrappers[scrapper]["function"]
-        scrapper_data = scrapper_function(address, chrome_options)
+    flattened_data = [item for sublist in data for item in sublist]
 
-        filename = f"{scrapper}-{address}.json"
-        print(f"saving data to {filename}")
+    df = pd.DataFrame(flattened_data)
+    if save_local_csv:
+        df.to_csv("../data/processed/data.csv", index_label=False)
 
-        save_raw_data(scrapper_data, filename)
-        data.extend(scrapper_data)
+    if save_local_json:
+        df.to_json("../data/processed/data.json", orient="index", indent=1)
 
-    df = pd.DataFrame(data)
-    print("saving data")
-    df.to_csv("../data/processed/data.csv", index_label=False)
-    df.to_json("../data/processed/data.json", orient="index", indent=1)
+    return flattened_data
+
+
+def collect_data_from_site(site_info: (str, dict)):
+    site = site_info[0]
+    site_dict = site_info[1]
+    address = site_dict.get("address")
+    chrome_options = site_dict.get("option")
+    scrapper_function = site_dict.get("function")
+    scrapper_data = scrapper_function(address, chrome_options)
+    filename = f"{site}-{address}.json"
+    save_raw_data(scrapper_data, filename)
+    return scrapper_data
