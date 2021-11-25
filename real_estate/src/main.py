0ca25025bebbc2765a3a0a87b50c1cb2da2e0b7c
gustavo <gugatessi@gmail.com>
2021-11-25 20:50:03 -0300

    Remove unnecessary code

--- real_estate/src/main.py
+++ real_estate/src/main.py
@@ -1,8 +1,7 @@
-import pandas as pd
-
 from real_estate.src.database.firebase_manager import get_firebase_data, upload_json_to_firebase, connect_to_firebase
 from real_estate.src.postprocess.coordinates import add_lat_lon_to_json
 from real_estate.src.collector import scrape_sites
+import pandas as pd
 
 
 class Collector:
@@ -26,7 +25,6 @@ class Collector:
         else:
             self._data = firebase_data
 
-
     @staticmethod
     def _data_in_firebase(data):
         return data is not None
@@ -38,9 +36,3 @@ class Collector:
     @property
     def data(self) -> pd.DataFrame:
         return pd.DataFrame(self._data)
-
-# if __name__ == "__main__":
-#     import json
-#     address = "Consolação, São Paulo"
-#     data = collect_data(address)
-#     print(json.dumps(data, indent=4, ensure_ascii=False))
