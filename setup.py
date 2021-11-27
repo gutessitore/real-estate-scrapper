64808caa71cf75af6e7828785371dddc09896804
gustavo <gugatessi@gmail.com>
2021-11-27 19:37:46 -0300

    Add package_data to setup

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
 from distutils.core import setup
+import os
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        if directories:
+            try:
+                directories.remove("__pycache__")
+            except ValueError:
+                pass
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+extra_files = package_files('real_estate/src')
 
 setup(
     name='real_estate',
     packages=['real_estate'],
-    version='0.0.0',
+    package_data={'': extra_files},
+    version='0.0.1',
     license='MIT',
     description='Package to scrape and analise data from real estate sites in Brazil',
     author='Gustavo Schlieper Tessitore',
     author_email='gugatessi@gmail.com',
     url='https://github.com/gutessitore/real-estate-scrapper',
-    download_url='https://github.com/gutessitore/real-estate-scrapper/archive/refs/tags/0.0.0.tar.gz',
+    download_url='https://github.com/gutessitore/real-estate-scrapper/archive/refs/tags/0.0.1.tar.gz',
     keywords=['real estate', 'rent', 'buy', 'web scrapper', 'selenium'],
     install_requires=[
         'pandas',
